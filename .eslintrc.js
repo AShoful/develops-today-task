ed32cf3298905df9ddf3d3a8933628e56080b849
Angrey Shoful <shoful.a@gmail.com>
2021-02-22 13:15:56 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/display-name": 0,
+        "react/react-in-jsx-scope": 0
+    }
+};
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Home = (): JSX.Element => (
+  <div> 
+    <Title>My First Next.js Page</Title>
+  </div>
+);
+
+
+const Title = styled.h1`
+  color: red;
+`;
+
+export default Home
